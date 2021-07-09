74650ad3e80482e9a970346536a027af38dd8d4a
Ruvick <rudikov.web@ya.ru>
2021-07-09 13:33:11 +0300

    Валидация

--- #source/js/main.js
+++ #source/js/main.js
@@ -1,6 +1,4 @@
-//@prepros-append script.js
 //@prepros-append jq-start.js
 //@prepros-append jquery.inputmask.bundle.min.js
 //@prepros-append jquery-script.js
-//@prepros-append forms.js
 //@prepros-append jq-end.js     
